abbeb4f6ec509ff4af88ec87f8326b588390910b
sk25469 <sk25469@gmail.com>
2021-10-25 14:39:23 +0530

    changes to product detail screen

--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -13,14 +13,42 @@ class ProductDetailScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ],
+            backgroundColor: Colors.black54,
             leading: IconButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              icon: const Icon(
-                Icons.chevron_left_outlined,
-                color: Colors.black,
-                size: 50,
+              icon: const CircleAvatar(
+                backgroundColor: Colors.black,
+                child: Icon(
+                  Icons.keyboard_backspace,
+                  color: Colors.white,
+                  size: 20,
+                ),
               ),
             ),
             expandedHeight: 300,
@@ -34,7 +62,7 @@ class ProductDetailScreen extends StatelessWidget {
                 child: Text(
                   product.title,
                   style: const TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                   ),
                 ),
               ),
@@ -50,27 +78,42 @@ class ProductDetailScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                const SizedBox(height: 10),
-                Text(
-                  '\$${product.price}',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 260,
+                      color: const Color.fromRGBO(35, 31, 32, 1),
+                      margin: const EdgeInsets.all(8.0),
+                      child: Text(
+                        ' An amazing motichur ka ${product.title}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                        // loadedProduct.description,
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topCenter,
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Rs.${product.price.ceil()}',
+                        style: const TextStyle(
+                          color: Colors.pinkAccent,
+                          fontSize: 45,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  width: double.infinity,
-                  child: Text(
-                    'An amazing ${product.title}',
-                    // loadedProduct.description,
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                  ),
-                ),
-                const SizedBox(height: 800),
               ],
             ),
           ),
